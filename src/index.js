7df888d76b454103e17c5f3ceb4d3830b2e64377
kitgelo <kitgelo@gmail.com>
2022-03-11 19:42:49 +0300

    fix: file index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0) {
+        let str = String(n).split('').reverse().join('');
+        return Number(str)
+    } else {
+        let str = String(n).slice(1).split('').reverse().join('');
+        return Number(str);
+    };
 }
