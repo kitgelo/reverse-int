0f4dbfb27f205f54b70f80abfc7218001f99c513
Oleg Nykytenko <kitgelo@gmail.com>
2023-08-02 12:04:54 +0300

    feat: new task solution

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 module.exports = function reverse (n) {
-    if (n > 0) {
-        let str = String(n).split('').reverse().join('');
-        return Number(str)
-    } else {
-        let str = String(n).slice(1).split('').reverse().join('');
-        return Number(str);
-    };
+    return String(Math.abs(n)).split('').reverse().join('')
 }
